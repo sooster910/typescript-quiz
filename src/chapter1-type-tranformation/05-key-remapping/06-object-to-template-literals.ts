4c3ec9a33dfcc200a9a1249ddd7ea0787bb64dc8
Hensley Joo <hs9880@gmail.com>
2025-09-27 17:57:31 +0900

    feat: Object key, value를 string union으로 변경

--- src/chapter1-type-tranformation/05-key-remapping/06-object-to-template-literals.ts
+++ src/chapter1-type-tranformation/05-key-remapping/06-object-to-template-literals.ts
@@ -5,8 +5,18 @@ interface FruitMap {
     banana: "yellow";
     orange: "orange";
 }
+//keyof FruitMap => "apple" | "banana" | "orange" //union type
+//FruitMap[K] => "red" | "yellow" | "orange"
 
-type TransformedFruit = unknown
+// in을 이용해 key를 순회하면서 value도 접근하도록 한다.
+//{[K in keyof FruitMap]: `${K}:${FruitMap[K]}`} => {
+//  apple: "apple:red";
+//  banana: "banana:yellow";
+//  orange: "orange:orange";
+//}
+
+
+type TransformedFruit = {[K in keyof FruitMap]: `${K}:${FruitMap[K]}`}[keyof FruitMap]
 
 type tests = [
     Expect<
