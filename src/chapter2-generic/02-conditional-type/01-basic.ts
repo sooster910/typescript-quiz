a7b1d25f3a796f503e6e5a79bf8882c603c3234b
Hensley Joo <hs9880@gmail.com>
2025-10-20 13:39:25 +0900

    feat: conditional type 연습

--- src/chapter2-generic/02-conditional-type/01-basic.ts
+++ src/chapter2-generic/02-conditional-type/01-basic.ts
@@ -1,6 +1,12 @@
 import {Equal, Expect} from "../../helper";
 
-type YouSayGoodbyeAndISayHello = unknown;
+/**
+ * hello를 generic으로 설정하면 타입추론이 goodbye가 나와야 하고
+ * goodbye를 generic으로 설정하면 타입추론이 hellow로 나와야 한다.
+ */
+
+type YouSayGoodbyeAndISayHello<T> = T extends "hello"?  "goodbye" :"hello";
+
 
 type tests = [
     Expect<Equal<YouSayGoodbyeAndISayHello<"hello">, "goodbye">>,
--- src/chapter2-generic/02-conditional-type/03-key-remapping.ts
+++ src/chapter2-generic/02-conditional-type/03-key-remapping.ts
@@ -8,7 +8,13 @@ interface Example {
     groupId: string;
 }
 
-type OnlyIdKeys<T> = unknown
+/**
+ * key가 id가 포함된 타입들을 추출해내기
+ */
+type OnlyIdKeys<T> = {
+    [K in keyof T as K extends `${string}${"id"|"Id"}`?K:never]:T[K]
+}
+type Confirm = OnlyIdKeys<Example>
 
 type tests = [
     Expect<
